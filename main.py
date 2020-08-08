73d5006b3d4bcc01571d0ec672b108636747fa6a
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2020-08-08 13:11:44 -0500

    Templates con Jinja 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
@@ -14,4 +14,5 @@ def index():
 @app.route('/hello')
 def hello():
   user_ip = request.cookies.get('user_ip')
-  return f'Hello, your IP is {user_ip}'
+  return render_template('hello.html', user_ip=user_ip)
+  
