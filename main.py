ba121c7386632e5ad353898b84bfa24e309d0ac6
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2020-08-08 13:03:47 -0500

    Reques and Response loops

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+  user_ip = request.remote_addr
+
+  response = make_response(redirect('/hello'))
+  response.set_cookie('user_ip', user_ip)
+
+  return response
+
+@app.route('/hello')
 def hello():
-  return 'Hello World Flask by SGG'
+  user_ip = request.cookies.get('user_ip')
+  return f'Hello, your IP is {user_ip}'
