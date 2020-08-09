11c21c069bf0cc0f35d956a9329198be0e2c623f
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2020-08-09 13:06:03 -0500

    Uso de metodo POST en Flask-WTF

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import secrets
-from flask import Flask, request, make_response, redirect, render_template, session
+from flask import Flask, request, make_response, redirect, render_template, session, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -13,8 +13,8 @@ app.config['SECRET_KEY'] = secrets.token_urlsafe()
 todos = ['Comprar cafe', 'Soliciutud de compra','Entregar el producto']
 
 class LoginForm(FlaskForm):
-  username = StringField('Nombre de usuario', validators=[DataRequired()])
-  password = PasswordField('Password', validators=[DataRequired()])
+  username = StringField('Nombre de usuario', validators=[DataRequired(), Length(min=5, max=30)])
+  password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
   submit = SubmitField('Enviar')
 
 @app.errorhandler(500)
@@ -35,13 +35,23 @@ def index():
 
   return response
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
   #user_ip = request.cookies.get('user_ip')
   user_ip = session.get('user_ip')
+  login_form = LoginForm()
+  username = session.get('username')
+
   context = {
     'user_ip': user_ip,
     'todos': todos,
-    'login_form': LoginForm()
+    'login_form': login_form,
+    'username': username
   }
+
+  if login_form.validate_on_submit():
+    username = login_form.username.data
+    session['username'] = username
+    return redirect(url_for('index'))
+
   return render_template('hello.html', **context)
