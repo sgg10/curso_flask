f38dab73edf49805759a6fd4bd58ce40680a57eb
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2020-08-10 17:44:40 -0500

    Implementacion de Firestore

---
+++ app/firestore_services.py
@@ -0,0 +1,16 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+credentials = credentials.ApplicationDefault()
+firebase_admin.initialize_app(credentials)
+
+db = firestore.client()
+
+def get_users():
+  return db.collection('users').get()
+
+def get_user(user_id):
+  return db.collection('users').document(user_id).get()
+
+def get_todos(user_id):
+  return db.collection('users').document(user_id).collection('todos').get()
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from flask import request, make_response, redirect, render_template, session, url_for, flash
 import unittest
+from flask_login import login_required
 
 from app import create_app
 from app.forms import LoginForm
+from app.firestore_services import get_users, get_todos
 
 app = create_app()
-
-todos = ['Comprar cafe', 'Soliciutud de compra','Entregar el producto']
-
 
 @app.cli.command()
 def test():
@@ -33,6 +32,7 @@ def index():
   return response
 
 @app.route('/hello', methods=['GET'])
+@login_required
 def hello():
   #user_ip = request.cookies.get('user_ip')
   user_ip = session.get('user_ip')
@@ -40,7 +40,7 @@ def hello():
 
   context = {
     'user_ip': user_ip,
-    'todos': todos,
+    'todos': get_todos(username),
     'username': username
   }
 
