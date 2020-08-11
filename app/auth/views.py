52b139984435f1d7e1020730c243d2e72a9f8061
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2020-08-10 21:11:21 -0500

    Login y Signup

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,10 @@
 from flask import render_template, session, redirect, url_for, flash
+from flask_login import login_user, login_required, logout_user
 from app.forms import LoginForm
 from . import auth
+from app.firestore_services import get_user, user_put
+from app.models import UserModel, UserData
+from werkzeug.security import generate_password_hash, check_password_hash
 
 @auth.route('/login', methods = ['POST', 'GET'])
 def login():
@@ -11,8 +15,61 @@ def login():
 
   if login_form.validate_on_submit():
     username = login_form.username.data
-    session['username'] = username
-    flash('Nombre de usuario registrado con éxito')
+    password = login_form.password.data
+
+    user_doc = get_user(username)
+
+    if user_doc.to_dict() is not None:
+      password_from_db = user_doc.to_dict()['password']
+      if check_password_hash(password_from_db, password):
+        user_data = UserData(username, password)
+        user = UserModel(user_data)
+        login_user(user)
+        flash('Bienvenido de nuevo')
+        redirect(url_for('hello'))
+      else:
+        flash('La informacion no coincide')
+    else:
+      flash('El usuario no existe')
+
     return redirect(url_for('index'))
-  
+
   return render_template('login.html', **context)
+
+@auth.route('signup', methods=['GET', 'POST'])
+def signup():
+  signup_form = LoginForm()
+
+  context = {
+    'signup_form': signup_form
+  }
+
+  if signup_form.validate_on_submit():
+    username = signup_form.username.data
+    password = signup_form.password.data
+
+    user_doc = get_user(username)
+
+    if user_doc.to_dict() is None:
+      password_hash = generate_password_hash(password)
+      user_data = UserData(username, password_hash)
+      user_put(user_data)
+
+      user = UserModel(user_data)
+      login_user(user)
+
+      flash('Bienvenido')
+
+      return redirect(url_for('hello'))
+    else: 
+      flash('El usuario ya existe')
+
+
+  return render_template('signup.html', **context)
+
+@auth.route('logout')
+@login_required
+def logout():
+  logout_user()
+  flash('Regresa pronto')
+  return redirect(url_for('auth.login'))
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import request, make_response, redirect, render_template, session, url_for, flash
 import unittest
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 from app import create_app
 from app.forms import LoginForm
@@ -36,8 +36,7 @@ def index():
 def hello():
   #user_ip = request.cookies.get('user_ip')
   user_ip = session.get('user_ip')
-  username = session.get('username')
-
+  username = current_user.id
   context = {
     'user_ip': user_ip,
     'todos': get_todos(username),
