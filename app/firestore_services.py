92def540c96a06526f5caac86790c635552dabae
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2020-08-14 17:04:32 -0500

    Delete and Update todo

--- app/firestore_services.py
+++ app/firestore_services.py
@@ -12,9 +12,26 @@ def get_users():
 def get_user(user_id):
   return db.collection('users').document(user_id).get()
 
-def get_todos(user_id):
-  return db.collection('users').document(user_id).collection('todos').get()
-
 def user_put(user_data):
   user_ref = db.collection('users').document(user_data.username)
   user_ref.set({ 'password': user_data.password })
+
+def get_todos(user_id):
+  return db.collection('users').document(user_id).collection('todos').get()
+
+def todo_put(user_id, description):
+  todo_ref = db.collection('users').document(user_id).collection('todos')
+  todo_ref.add({ 'description': description, 'done': False })
+
+def delete_todo(user_id, todo_id):
+  # ref = db.collection('users').document(user_id).collection('todos').document(todo_id)
+  todo_ref = _get_todo_ref(user_id, todo_id)
+  todo_ref.delete()
+
+def update_todo(user_id, todo_id, done):
+  todo_done = not bool(done)
+  todo_ref = _get_todo_ref(user_id, todo_id)
+  todo_ref.update({ 'done': todo_done })
+
+def _get_todo_ref(user_id, todo_id):
+  return db.document(f'users/{user_id}/todos/{todo_id}')
--- app/forms.py
+++ app/forms.py
@@ -6,3 +6,13 @@ class LoginForm(FlaskForm):
   username = StringField('Nombre de usuario', validators=[DataRequired(), Length(min=5, max=30)])
   password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
   submit = SubmitField('Enviar')
+
+class TodoForm(FlaskForm):
+  description = StringField('Descripción', validators=[DataRequired()])
+  submit_field = SubmitField('Crear')
+
+class DeleteTodoForm(FlaskForm):
+  submit = SubmitField('Borrar')
+
+class UpdateTodoForm(FlaskForm):
+  submit = SubmitField('Actualizar')
